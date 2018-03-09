8c87a91e4da0f52ddbc88270e6b0c51ec26297e1
wy <ningwy1314@163.com>
2018-03-09 18:03:33 +0800

    加密未完成-AES解密异常

--- app/src/main/java/com/wy/work/MainActivity.java
+++ app/src/main/java/com/wy/work/MainActivity.java
@@ -2,7 +2,6 @@ package com.wy.work;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
@@ -11,8 +10,6 @@ import com.wy.work.dialog.LoginDialogFragment;
 import com.wy.work.dialog.SetNameDialogFragment;
 import com.wy.work.encryption.EncryptionUtil;
 
-import java.util.Arrays;
-
 public class MainActivity extends AppCompatActivity implements LoginDialogFragment.LoginInputCompleteListener {
 
     @Override
@@ -20,11 +17,25 @@ public class MainActivity extends AppCompatActivity implements LoginDialogFragme
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.e("wy__base64_密文: ", EncryptionUtil.base64ToCode("待加密文字"));
+        Log.e("wy__base64_密文: ", EncryptionUtil.codeToBase64("待加密文字"));
 
-        Log.e("wy__base64_明文: ", EncryptionUtil.base64ToDecode("5b6F5Yqg5a+G5paH5a2X"));
+        Log.e("wy__base64_明文: ", EncryptionUtil.decodeToBase64("5b6F5Yqg5a+G5paH5a2X"));
 
-        Log.e("wy__base64_密文: ", EncryptionUtil.md5ToCode("待加密文字"));
+        Log.e("wy__md5_密文: ", EncryptionUtil.codeToMD5("待加密文字"));
+
+        String secretKey = EncryptionUtil.generateKey();
+        String codeToAES = EncryptionUtil.codeToAES("key","aaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+        Log.e("wy__aes_密文: ", codeToAES);
+
+        Log.e("wy__aes_明文: ", EncryptionUtil.decodeToAes("key",codeToAES));
+
+        try {
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void setNameDialogFragment(View view) {
--- app/src/main/java/com/wy/work/encryption/EncryptionUtil.java
+++ app/src/main/java/com/wy/work/encryption/EncryptionUtil.java
@@ -1,11 +1,25 @@
 package com.wy.work.encryption;
 
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
 import android.util.Base64;
 
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 
 /**
@@ -20,7 +34,7 @@ public class EncryptionUtil {
      * @param express 原文
      * @return 密文
      */
-    public static String base64ToCode(String express) {
+    public static String codeToBase64(String express) {
 
         return Base64.encodeToString(express.getBytes(), Base64.DEFAULT);
     }
@@ -32,7 +46,7 @@ public class EncryptionUtil {
      * @return 原文
      */
     @NonNull
-    public static String base64ToDecode(String cipherText) {
+    public static String decodeToBase64(String cipherText) {
 
         return new String(Base64.decode(cipherText, Base64.DEFAULT));
     }
@@ -43,7 +57,7 @@ public class EncryptionUtil {
      * @param express 原文
      * @return 密文
      */
-    public static String md5ToCode(String express) {
+    public static String codeToMD5(String express) {
 
         if (TextUtils.isEmpty(express)) {
 
@@ -53,9 +67,10 @@ public class EncryptionUtil {
         MessageDigest messageDigest = null;
 
         try {
-            // 获得密文完成哈希计算,产生128 位的长整数
+
             messageDigest = MessageDigest.getInstance("MD5");
 
+            // 产生的散列值的字节数组。
             byte[] bytes = messageDigest.digest(express.getBytes());
 
             StringBuilder result = new StringBuilder();
@@ -80,5 +95,167 @@ public class EncryptionUtil {
 
             return "";
         }
+    }
+
+
+    //常量介绍
+    private final static String HEX = "0123456789ABCDEF";
+    //AES是加密方式 CBC是工作模式 PKCS5Padding是填充模式
+    private static final String CBC_PKCS5_PADDING = "AES/CBC/PKCS5Padding";
+    //AES 加密
+    private static final String AES = "AES";
+    // SHA1PRNG 强随机种子算法, 要区别4.2以上版本的调用方法
+    private static final String SHA1PRNG = "SHA1PRNG";
+
+    public static String generateKey() {
+
+        try {
+
+            SecureRandom localSecureRandom = SecureRandom.getInstance(SHA1PRNG);
+
+            byte[] bytesKey = new byte[20];
+
+            localSecureRandom.nextBytes(bytesKey);
+
+            return toHex(bytesKey);
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    //二进制转字符
+    public static String toHex(byte[] buf) {
+        if (buf == null)
+            return "";
+        StringBuffer result = new StringBuffer(2 * buf.length);
+        for (int i = 0; i < buf.length; i++) {
+            appendHex(result, buf[i]);
+        }
+        return result.toString();
+    }
+
+    private static void appendHex(StringBuffer sb, byte b) {
+        sb.append(HEX.charAt((b >> 4) & 0x0f)).append(HEX.charAt(b & 0x0f));
+    }
+
+    public static byte[] getRawKey(byte[] seed) {
+
+        try {
+
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+
+            //for android // 在4.2以上版本中，SecureRandom获取方式发生了改变
+            SecureRandom secureRandom =   secureRandom = SecureRandom.getInstance(SHA1PRNG);
+
+            //设置随机生成器的种子
+            secureRandom.setSeed(seed);
+
+            //AES中128位密钥版本有10个加密循环，192比特密钥版本有12个加密循环，256比特密钥版本则有14个加密循环。
+            keyGenerator.init(128, secureRandom);
+
+            //生成一个密钥
+            SecretKey secretKey = keyGenerator.generateKey();
+
+            //获得密钥的字节数组,以其主编码格式返回
+            return secretKey.getEncoded();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static String codeToAES(String key, String cleartext)  {
+
+        if (TextUtils.isEmpty(cleartext)) {
+
+            return "";
+        }
+
+        byte[] raw = getRawKey(key.getBytes());
+
+        SecretKeySpec secretKeySpec = new SecretKeySpec(raw, "AES");
+
+        Cipher cipher = null;
+        byte[] encrypted = null;
+        try {
+            cipher = Cipher.getInstance(CBC_PKCS5_PADDING);
+
+            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, new IvParameterSpec(new byte[cipher.getBlockSize()]));
+
+            encrypted = cipher.doFinal(cleartext.getBytes());
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        }finally {
+
+            return Base64Encoder.encode(encrypted);
+        }
+
+
+
+    }
+
+    public static String decodeToAes(String key, String encrypted)  {
+
+        if (TextUtils.isEmpty(encrypted)) {
+            return encrypted;
+        }
+
+        byte[] enc = Base64Decoder.decodeToBytes(encrypted);
+        byte[] result = new byte[0];
+        try {
+
+            result = decrypt(key, enc);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new String(result);
+    }
+
+    private static byte[] decrypt(String key, byte[] encrypted) {
+        byte[] raw = getRawKey(key.getBytes());
+        SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+        Cipher cipher = null;
+        byte[] decrypted = null;
+        try {
+            cipher = Cipher.getInstance(CBC_PKCS5_PADDING);
+
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec, new IvParameterSpec(new byte[cipher.getBlockSize()]));
+            decrypted = cipher.doFinal(encrypted);
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        } finally {
+
+            return decrypted;
+        }
+
+
     }
 }
